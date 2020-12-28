c7df382ffb854422203790d405133971e0073de9
David Billsbrough <kc4zvw@users.noreply.github.com>
2020-12-28 02:10:30 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+#/usr/bin/env python3
+#
+# $Id:$
+
+# First things, first. Import the wxPython package.
+import wx
+
+# Next, create an application object.
+app = wx.App()
+
+# Then a frame.
+frm = wx.Frame(None, title="Hello Big Blue Bad World")
+
+# Show it.
+frm.Show()
+
+# Start the event loop.
+app.MainLoop()
+
+# end of app
